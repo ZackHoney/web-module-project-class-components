25dbefde9ede53f39403b02acdc72f16b12c060d
Zachary Honey <zhoney466@gmail.com>
2023-07-06 21:33:35 -0600

    Build from state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,14 +1,34 @@
 import React from 'react'
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Organize Garage',
+          id: 1528817077286, 
+          completed: false
+        },
+        {
+          name: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
+    }
+  }
   render() {
+    const { todos } = this.state;
     return (
       <div>
         <h1>To Do's</h1>
         <ul>
-          <li>Walk the dog</li>
-          <li>Take out trash</li>
-          <li>Workout</li>
+          {
+            todos.map(todo=> {
+              return (<li>{todo.name} { todo.completed?<span>- completed</span> : <span></span> }</li>)
+            })
+          }
         </ul>
 
         <form>
